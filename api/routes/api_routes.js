2b7b8b8eda875305168e4822354a0ecbfbf316e0
michael nakashima <michael.nakashima-ext@ab-inbev.com>
2022-09-12 10:52:58 -0300

    [NODE] updating routes

--- api/routes/api_routes.js
+++ api/routes/api_routes.js
@@ -1,59 +1,101 @@
-const express = require ('express') 
+const express = require('express')
+const app = express()
+const port = 3000
 
-let apiRouter = express.Router() 
- 
-const endpoint = '/' 
-const lista_produtos = { 
-    produtos: [ 
-        { id: 1, descricao: "Arroz parboilizado 5Kg", valor: 25.00, marca: "Tio João"  },
-        { id: 2, descricao: "Maionese 250gr", valor: 7.20, marca: "Helmans"  },
-        { id: 3, descricao: "Iogurte Natural 200ml", valor: 2.50, marca: "Itambé"  },
-        { id: 4, descricao: "Batata Maior Palha 300gr", valor: 15.20, marca: "Chipps"  },
-        { id: 5, descricao: "Nescau 400gr", valor: 8.00, marca: "Nestlé"  },
-    ] 
-} 
- 
-apiRouter.get (endpoint + 'produtos', function (req, res) { 
-    res.status(200).json (lista_produtos) 
+let apiRouter = express.Router()
+
+const endpoint = '/'
+const lista_produtos = {
+    produtos: [{
+            id: 1,
+            descricao: "Arroz parboilizado 5Kg",
+            valor: 25.00,
+            marca: "Tio João"
+        },
+        {
+            id: 2,
+            descricao: "Maionese 250gr",
+            valor: 7.20,
+            marca: "Helmans"
+        },
+        {
+            id: 3,
+            descricao: "Iogurte Natural 200ml",
+            valor: 2.50,
+            marca: "Itambé"
+        },
+        {
+            id: 4,
+            descricao: "Batata Maior Palha 300gr",
+            valor: 15.20,
+            marca: "Chipps"
+        },
+        {
+            id: 5,
+            descricao: "Nescau 400gr",
+            valor: 8.00,
+            marca: "Nestlé"
+        },
+    ]
+}
+
+app.get(endpoint, function(req, res){
+    res.send('index');
 }) 
 
-apiRouter.get (endpoint + 'produtos/:id', function (req, res) { 
+app.get(endpoint + 'produtos', function (req, res) {
+    res.status(200).json(lista_produtos)
+})
+
+app.get(endpoint + 'produtos/:id', function (req, res) {
     const product = lista_produtos.produtos.find(p => p.id == req.params.id)
     if (!product) {
-        return res.status(404).json();    
+        return res.status(404).json();
     }
-    res.status(200).json (product) 
-}) 
+    res.status(200).json(product)
+})
 
-apiRouter.put (endpoint + 'produtos/:id', function (req, res) { 
+app.put(endpoint + 'produtos/:id', function (req, res) {
     const product = req.body;
     const id = req.params.id;
 
     const old_product = lista_produtos.produtos.find(p => p.id == req.params.id)
     const index = lista_produtos.produtos.indexOf(old_product);
 
-    lista_produtos.produtos[index] = {...product, id } ;
+    lista_produtos.produtos[index] = {
+        ...product,
+        id
+    };
 
-    res.status(200).json (product) 
-}) 
+    res.status(200).json(product)
+})
 
-apiRouter.delete (endpoint + 'produtos/:id', function (req, res) { 
+app.delete(endpoint + 'produtos/:id', function (req, res) {
     const old_product = lista_produtos.produtos.find(p => p.id == req.params.id)
     const index = lista_produtos.produtos.indexOf(old_product);
 
-    if (index > -1) { 
+    if (index > -1) {
         lista_produtos.produtos.splice(index, 1);
-      }
+    }
 
-    res.status(204).json () 
-}) 
+    res.status(204).json()
+})
 
-apiRouter.post (endpoint + 'produtos', function (req, res) { 
+app.post(endpoint + 'produtos', function (req, res) {
     const product = req.body;
     const id = lista_produtos.produtos.length + 1;
-    lista_produtos.produtos.push({ ...product, id });
-    res.status(200).json (lista_produtos) 
-}) 
+    lista_produtos.produtos.push({
+        ...product,
+        id
+    });
+    res.status(200).json(lista_produtos)
+})
 
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+})
 
-module.exports = apiRouter; 
+module.exports = apiRouter;
