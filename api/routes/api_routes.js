7b91f7f19b4253dc9e6ba100cce86d6af4f7b9e8
michael nakashima <michael.nakashima-ext@ab-inbev.com>
2022-09-12 10:31:45 -0300

    [Node] display correct product

--- api/routes/api_routes.js
+++ api/routes/api_routes.js
@@ -5,9 +5,11 @@ let apiRouter = express.Router()
 const endpoint = '/' 
 const lista_produtos = { 
     produtos: [ 
-        { id: 1, descricao: "Produto 1", valor: 15.00, marca: "A"  }, 
-        { id: 2, descricao: "Produto 2", valor: 25.00, marca: "B"  }, 
-        { id: 3, descricao: "Produto 3", valor: 35.00, marca: "C"  }, 
+        { id: 1, descricao: "Arroz parboilizado 5Kg", valor: 25.00, marca: "Tio João"  },
+        { id: 2, descricao: "Maionese 250gr", valor: 7.20, marca: "Helmans"  },
+        { id: 3, descricao: "Iogurte Natural 200ml", valor: 2.50, marca: "Itambé"  },
+        { id: 4, descricao: "Batata Maior Palha 300gr", valor: 15.20, marca: "Chipps"  },
+        { id: 5, descricao: "Nescau 400gr", valor: 8.00, marca: "Nestlé"  },
     ] 
 } 
  
