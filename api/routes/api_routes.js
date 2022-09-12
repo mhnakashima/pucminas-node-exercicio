7e22ce77d9ea85754bb3c076dc805eb77930cea8
michael nakashima <michael.nakashima-ext@ab-inbev.com>
2022-09-12 11:00:53 -0300

    [NODE] updating routes

--- api/routes/api_routes.js
+++ api/routes/api_routes.js
@@ -91,7 +91,7 @@ app.post(endpoint + 'produtos', function (req, res) {
     res.status(200).json(lista_produtos)
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(3000, function () {
     var host = server.address().address
     var port = server.address().port
     
